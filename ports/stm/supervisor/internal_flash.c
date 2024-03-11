043664258ffd795f2549500828a2f2db9cb91bcc
snkYmkrct <snkYmkrct@gmail.com>
2024-03-11 11:23:12 +0100

    flash bank change

--- ports/stm/supervisor/internal_flash.c
+++ ports/stm/supervisor/internal_flash.c
@@ -88,7 +88,10 @@ static uint32_t _cache_flash_addr = NO_CACHE;
 #if defined(STM32H7)
 // get the bank of a given flash address
 STATIC uint32_t get_bank(uint32_t addr) {
-    /* 
+    #if defined(STM32H750xx) // H750 only has 1 bank
+        return FLASH_BANK_1;
+    #else
+     
     if (READ_BIT(FLASH->OPTCR, FLASH_OPTCR_SWAP_BANK) == 0) {
         // no bank swap
         if (addr < (FLASH_BASE + FLASH_BANK_SIZE)) {
@@ -103,7 +106,8 @@ STATIC uint32_t get_bank(uint32_t addr) {
         } else {
             return FLASH_BANK_1;
         }
-    }*/ return FLASH_BANK_1; 
+    } 
+    #endif
 } 
 #endif
 
