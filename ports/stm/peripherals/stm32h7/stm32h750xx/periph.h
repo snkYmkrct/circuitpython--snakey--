1dd920ef4132c71925dc41f9926c2d0384fcddcf
snkymkrct <snkymkrct@gmail.com>
2025-03-28 23:15:57 +0100

    Add rest of board pin definitions and peripheral pin cleanup

--- ports/stm/peripherals/stm32h7/stm32h750xx/periph.h
+++ ports/stm/peripherals/stm32h7/stm32h750xx/periph.h
@@ -7,24 +7,24 @@
 #pragma once
 
 // I2C
-extern I2C_TypeDef *mcu_i2c_banks[4];
+extern I2C_TypeDef *mcu_i2c_banks[MAX_I2C];
 
 extern const mcu_periph_obj_t mcu_i2c_sda_list[12];
 extern const mcu_periph_obj_t mcu_i2c_scl_list[12];
 
 // SPI
-extern SPI_TypeDef *mcu_spi_banks[6];
+extern SPI_TypeDef *mcu_spi_banks[MAX_SPI];
 
-extern const mcu_periph_obj_t mcu_spi_sck_list[19];
-extern const mcu_periph_obj_t mcu_spi_mosi_list[19];
-extern const mcu_periph_obj_t mcu_spi_miso_list[16];
+extern const mcu_periph_obj_t mcu_spi_sck_list[18];
+extern const mcu_periph_obj_t mcu_spi_mosi_list[18];
+extern const mcu_periph_obj_t mcu_spi_miso_list[15];
 
 // UART
 extern USART_TypeDef *mcu_uart_banks[MAX_UART];
 extern bool mcu_uart_has_usart[MAX_UART];
 
-extern const mcu_periph_obj_t mcu_uart_tx_list[25];
-extern const mcu_periph_obj_t mcu_uart_rx_list[26];
+extern const mcu_periph_obj_t mcu_uart_tx_list[24];
+extern const mcu_periph_obj_t mcu_uart_rx_list[25];
 
 // Timers
 #define TIM_BANK_ARRAY_LEN 14
